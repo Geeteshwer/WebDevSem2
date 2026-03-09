eb17f2b00e15e174f9929ab790ac359b996949db
Geeteshwer <geeteshwers@gmail.com>
2026-03-09 09:39:48 +0530

    Add files via upload

---
+++ loop.js
@@ -0,0 +1,15 @@
+let f = 5
+for(let i = 1;i<=f;i++){
+    console.log(i);
+}
+while(f>0){
+    console.log("Hello World");
+    f--;    
+}
+let t= 3
+do{
+    console.log("Hello");
+    t--;  }
+while(t>0){
+    
+}
